34e50649b309781ab206e3d4605efddfb41300d7
abhishek90900 <mukherjeeabhishek613@gmail.com>
2025-03-02 23:45:37 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Add scroll event listener to the window
-window.addEventListener('scroll', function() {
-    const nav = document.querySelector('nav');
-    if (window.scrollY > 50) {
-        nav.classList.add('scrolled');
-    } else {
-        nav.classList.remove('scrolled');
-    }
-});
